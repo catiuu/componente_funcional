e26630d47043cce116feb16b81504a894d04faaf
Nascimento <Catiussia.Nascimento@btgpactual.com>
2025-08-22 15:40:10 -0300

    teste 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import FunctionalComponent from "./components/Header/Header";
 function App() {
   return (
     <div>
+      <h1>Welcome to My App</h1>
       <FunctionalComponent />
     </div>
   );
